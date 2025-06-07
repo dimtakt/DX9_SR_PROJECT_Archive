a0cd6e1fb752db46da29fd65cca369750093d80c
kimbumsu0312 <91525897+kimbumsu0312@users.noreply.github.com>
2025-06-07 21:06:00 +0900

    아이템 정보 창

--- Framework/Client/Private/Artefact_Tooltip.cpp
+++ Framework/Client/Private/Artefact_Tooltip.cpp
@@ -2,6 +2,7 @@
 #include "GameInstance.h"
 #include "Item_Tooltip_Mid.h"
 #include "Item_Tooltip_Bottom.h"
+#include "Button.h"
 CArtefact_Tooltip::CArtefact_Tooltip(LPDIRECT3DDEVICE9 pGraphic_Device) : CTooltip(pGraphic_Device)
 {
 }
@@ -122,11 +123,13 @@ HRESULT CArtefact_Tooltip::Ready_Children()
 {
 	CUIObject* pGameObject = nullptr;
 
-	pGameObject = dynamic_cast<CUIObject*>(m_pGameInstance->Clone_Prototype(PROTOTYPE::GAMEOBJECT, ENUM_CLASS(m_eLevel), TEXT("Prototype_GameObject_UI_ArteFact_Tooltip_Mid")));
+	UIOBJECT_DESC Desc{};
+	Desc.fZ = 0;
+	pGameObject = dynamic_cast<CUIObject*>(m_pGameInstance->Clone_Prototype(PROTOTYPE::GAMEOBJECT, ENUM_CLASS(m_eLevel), TEXT("Prototype_GameObject_UI_ArteFact_Tooltip_Mid"), &Desc));
 	if (nullptr == pGameObject)
 		return E_FAIL;
 	Add_Child(pGameObject);
-	pGameObject = dynamic_cast<CUIObject*>(m_pGameInstance->Clone_Prototype(PROTOTYPE::GAMEOBJECT, ENUM_CLASS(m_eLevel), TEXT("Prototype_GameObject_UI_ArteFact_Tooltip_Bottom")));
+	pGameObject = dynamic_cast<CUIObject*>(m_pGameInstance->Clone_Prototype(PROTOTYPE::GAMEOBJECT, ENUM_CLASS(m_eLevel), TEXT("Prototype_GameObject_UI_ArteFact_Tooltip_Bottom"), &Desc));
 	if (nullptr == pGameObject)
 		return E_FAIL;
 	Add_Child(pGameObject);
@@ -136,22 +139,15 @@ HRESULT CArtefact_Tooltip::Ready_Children()
 
 void CArtefact_Tooltip::Render_Font()
 {
+	TCHAR szText[MAX_PATH];
+	D3DXCOLOR TexColor{};
+
 	Font_Rect_Update();
 	CUIObject::Font_Rect_Update();
 
 	_uint iItemID = m_pItemObject->Item_Info()->iItemID;
 
 	m_vTexRect.left -= 30;
-	m_vTexRect.right -= 30;
-	m_pGameInstance->Render_Font(TEXT("UI_Font_22_Damage"), g_ItemDataBase[iItemID].m_szName, m_vTexRect, D3DXCOLOR(1.f, 1.f, 1.f, 1.f), DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-	
-	CUIObject::Font_Rect_Update();
-	m_vTexRect.top += 220;
-	m_vTexRect.left += 50;
-	TCHAR szText[MAX_PATH];
-	D3DXCOLOR TexColor{};
-
-
 	switch (g_ItemDataBase[iItemID].m_eRarity)
 	{
 	case ITEM_RARITY::NORMAL:
@@ -171,25 +167,41 @@ void CArtefact_Tooltip::Render_Font()
 		TexColor = D3DXCOLOR(1.f, 1.f, 0.f, 1.f);
 		break;
 	}
+	m_vTexRect.right -= 30;
+	m_pGameInstance->Render_Font(TEXT("UI_Font_22_Damage"), g_ItemDataBase[iItemID].m_szName, m_vTexRect, TexColor, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	
+	CUIObject::Font_Rect_Update();
+	m_vTexRect.top += 220;
+	m_vTexRect.left += 50;
 
 	m_pGameInstance->Render_Font(TEXT("UI_Font_16_Tooltip"), szText, m_vTexRect, TexColor, DT_LEFT | DT_TOP);
 
-	if (g_ItemDataBase[iItemID].m_eType == ITEM_TYPE::ARTEFACT)
+	if (g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_eType == ITEM_EFFECT::VALUE_TYPE)
 	{
 		for (_int i = 0; i < g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_vecValue.size(); ++i)
 		{
 			m_vTexRect.top += 20;
 			_stprintf_s(szText, TEXT(""));
-	//		_tcscpy_s(szText, g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_vecValue[i].m_eStat);
+			_tcscpy_s(szText, g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_vecValue[i].m_szText.c_str());
+			
+			_float iStat_Value = g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_vecValue[i].m_fStat_Value1;
+			_float iUP_Value = g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_vecValue[i].m_fStat_Value2;
 
-			_wstring szSlay = TEXT(" - ");
-			_int iTemp = g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_vecValue[0].m_fStat_Value1;
+			iStat_Value = iStat_Value + iUP_Value * static_cast<CButton*>(m_pParent)->Get_Value();
+			
+			_stprintf_s(szText, szText, int(iStat_Value));
 
-			//_wstring szSlay = g_ItemDataBase[iItemID].m_szDescription;
-
-			m_pGameInstance->Render_Font(TEXT("UI_Font_16_Tooltip"), szSlay, m_vTexRect, D3DXCOLOR(1.f, 1.f, 1.f, 1.f), DT_LEFT | DT_TOP);
+			m_pGameInstance->Render_Font(TEXT("UI_Font_16_Tooltip"), szText, m_vTexRect, D3DXCOLOR(1.f, 1.f, 1.f, 1.f), DT_LEFT | DT_TOP);
 		}
 	}
+	else
+	{
+		m_vTexRect.top += 20;
+		m_pGameInstance->Render_Font(TEXT("UI_Font_16_Tooltip"), g_ItemEffect[g_ItemDataBase[iItemID].m_iARTEFACT_Value].m_vecValue[0].m_szText, m_vTexRect, D3DXCOLOR(1.f, 1.f, 1.f, 1.f), DT_LEFT | DT_TOP);
+	}
+
+	m_vTexRect.top += 70;
+	m_pGameInstance->Render_Font(TEXT("UI_Font_16_Tooltip"), g_ItemDataBase[iItemID].m_szDescription, m_vTexRect, D3DXCOLOR(0.5f, 0.5f, 0.5f, 1.f), DT_LEFT | DT_TOP);
 	return;
 
 }
--- Framework/Client/Private/Slate_Tooltip.cpp
+++ Framework/Client/Private/Slate_Tooltip.cpp
@@ -1,11 +1,14 @@
 #include "Slate_Tooltip.h"
 #include "GameInstance.h"
+#include "Item_Tooltip_Mid.h"
+#include "Item_Tooltip_Bottom.h"
+#include "Button.h"
 
-CSlate_Tooltip::CSlate_Tooltip(LPDIRECT3DDEVICE9 pGraphic_Device) : CUIObject(pGraphic_Device)
+CSlate_Tooltip::CSlate_Tooltip(LPDIRECT3DDEVICE9 pGraphic_Device) : CTooltip(pGraphic_Device)
 {
 }
 
-CSlate_Tooltip::CSlate_Tooltip(const CSlate_Tooltip& Prototype) : CUIObject(Prototype), m_eLevel(Prototype.m_eLevel)
+CSlate_Tooltip::CSlate_Tooltip(const CSlate_Tooltip& Prototype) : CTooltip(Prototype), m_eLevel(Prototype.m_eLevel)
 {
 }
 
@@ -13,15 +16,17 @@ HRESULT CSlate_Tooltip::Initialize_Prototype(LEVEL eLevel)
 {
 	m_eLevel = eLevel;
 
+	if (FAILED(Ready_ChildPrototype(m_eLevel)))
+		return E_FAIL;
+
 	return S_OK;
 }
 
 HRESULT CSlate_Tooltip::Initialize(void* pArg)
 {
-
-	m_fSizeX = 470;
-	m_fSizeY = 470;
-	m_fX = 0;
+	m_fSizeX = 350;
+	m_fSizeY = 350;
+	m_fX = 195;
 	m_fY = 0;
 	m_fZ = UI_DEPTH::TOOLTIP;
 	m_iWinSizeX = g_iWinSizeX;
@@ -35,6 +40,9 @@ HRESULT CSlate_Tooltip::Initialize(void* pArg)
 
 	m_pTransformCom->Scaling(m_fSizeX, m_fSizeY, 1.f);
 	__super::Update_Position();
+
+	if (FAILED(Ready_Children()))
+		return E_FAIL;
 
 	return S_OK;
 }
@@ -52,6 +60,17 @@ void CSlate_Tooltip::Update(_float fTimeDelta)
 void CSlate_Tooltip::Late_Update(_float fTimeDelta)
 {
 	__super::Late_Update(fTimeDelta);
+
+	m_pGameInstance->Add_RenderGroup(RENDERGROUP::RG_UI_BLEND, this);
+
+	_float3 vRenderPos;
+	vRenderPos = m_pTransformCom->Get_State(STATE::POSITION);
+
+	vRenderPos.x += 105;
+	vRenderPos.z = 0;
+
+	if (m_pItemObject != nullptr)
+		static_cast<CItem_Base*>(m_pItemObject)->IsTooltip(vRenderPos);
 }
 
 HRESULT CSlate_Tooltip::Render()
@@ -62,6 +81,7 @@ HRESULT CSlate_Tooltip::Render()
 
 	__super::Begin();
 	m_pVIBufferCom->Render();
+	Render_Font();
 	return S_OK;
 }
 
@@ -80,6 +100,78 @@ HRESULT CSlate_Tooltip::Ready_Components()
 		return E_FAIL;
 
 	return S_OK;
+}
+
+HRESULT CSlate_Tooltip::Ready_ChildPrototype(LEVEL eLevel)
+{
+	if (FAILED(m_pGameInstance->Add_Prototype(ENUM_CLASS(eLevel), TEXT("Prototype_GameObject_UI_Slate_Tooltip_Mid"),
+		CItem_Tooltip_Mid::Create(m_pGraphic_Device, eLevel))))
+		return E_FAIL;
+
+	if (FAILED(m_pGameInstance->Add_Prototype(ENUM_CLASS(eLevel), TEXT("Prototype_GameObject_UI_Slate_Tooltip_Bottom"),
+		CItem_Tooltip_Bottom::Create(m_pGraphic_Device, eLevel))))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CSlate_Tooltip::Ready_Children()
+{
+	CUIObject* pGameObject = nullptr;
+
+	UIOBJECT_DESC Desc{};
+	Desc.fZ = 1;
+	pGameObject = dynamic_cast<CUIObject*>(m_pGameInstance->Clone_Prototype(PROTOTYPE::GAMEOBJECT, ENUM_CLASS(m_eLevel), TEXT("Prototype_GameObject_UI_ArteFact_Tooltip_Mid"), &Desc));
+	if (nullptr == pGameObject)
+		return E_FAIL;
+	Add_Child(pGameObject);
+	pGameObject = dynamic_cast<CUIObject*>(m_pGameInstance->Clone_Prototype(PROTOTYPE::GAMEOBJECT, ENUM_CLASS(m_eLevel), TEXT("Prototype_GameObject_UI_ArteFact_Tooltip_Bottom"), &Desc));
+	if (nullptr == pGameObject)
+		return E_FAIL;
+	Add_Child(pGameObject);
+
+	return S_OK;
+}
+
+void CSlate_Tooltip::Render_Font()
+{
+	TCHAR szText[MAX_PATH];
+	D3DXCOLOR TexColor{};
+
+	Font_Rect_Update();
+	CUIObject::Font_Rect_Update();
+
+	_uint iItemID = m_pItemObject->Item_Info()->iItemID;
+
+	m_vTexRect.left -= 30;
+	switch (g_ItemDataBase[iItemID].m_eRarity)
+	{
+	case ITEM_RARITY::NORMAL:
+		_stprintf_s(szText, TEXT("[ÀÏ¹Ý]"));
+		TexColor = D3DXCOLOR(1.f, 1.f, 1.f, 1.f);
+		break;
+	case ITEM_RARITY::RARE:
+		_stprintf_s(szText, TEXT("[°í±Þ]"));
+		TexColor = D3DXCOLOR(0.f, 1.f, 0.f, 1.f);
+		break;
+	case ITEM_RARITY::EPIC:
+		_stprintf_s(szText, TEXT("[Èñ±Í]"));
+		TexColor = D3DXCOLOR(0.f, 0.f, 1.f, 1.f);
+		break;
+	case ITEM_RARITY::LEGENDARY:
+		_stprintf_s(szText, TEXT("[Àü¼³]"));
+		TexColor = D3DXCOLOR(1.f, 1.f, 0.f, 1.f);
+		break;
+	}
+	m_vTexRect.right -= 30;
+	m_pGameInstance->Render_Font(TEXT("UI_Font_22_Damage"), g_ItemDataBase[iItemID].m_szName, m_vTexRect, TexColor, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+	CUIObject::Font_Rect_Update();
+	m_vTexRect.top += 220;
+	m_vTexRect.left += 50;
+
+	m_pGameInstance->Render_Font(TEXT("UI_Font_16_Tooltip"), szText, m_vTexRect, TexColor, DT_LEFT | DT_TOP);
+
 }
 
 CSlate_Tooltip* CSlate_Tooltip::Create(LPDIRECT3DDEVICE9 pGraphic_Device, LEVEL eLevel)
