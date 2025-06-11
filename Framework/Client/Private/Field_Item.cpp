f066fa33fb51350220127cb8c0c2d29e35e80b4c
lasval <lasval39@gmail.com>
2025-06-11 20:27:43 +0900

    FEAT: 충돌시에만 아이템 정보 뜰수있게 처리

--- Framework/Client/Private/Field_Item.cpp
+++ Framework/Client/Private/Field_Item.cpp
@@ -11,7 +11,7 @@ CField_Item::CField_Item(const CField_Item& Prototype) : CUIObject(Prototype), m
 {
 }
 
-void CField_Item::Render_Field_Item(_float fTimeDelta, _bool bFontRender)
+void CField_Item::Render_Field_Item(_float fTimeDelta)
 {
 	m_fAccTime += fTimeDelta;
 	_float Offset = sinf(m_fAccTime * m_fSpeed) * m_fRange;
@@ -19,8 +19,6 @@ void CField_Item::Render_Field_Item(_float fTimeDelta, _bool bFontRender)
 	m_pTransformCom->Set_State(STATE::POSITION, m_vTargetPos);
 	m_pGameInstance->Add_RenderGroup(RENDERGROUP::RG_UI, this);
 
-	if(bFontRender)
-		__super::Late_Update(fTimeDelta);
 }
 
 void CField_Item::Buy_Item()
@@ -147,6 +145,15 @@ HRESULT CField_Item::Ready_Children()
 	Add_Child(pGameObject);
 
 	return S_OK;
+}
+
+void CField_Item::OnCollision(CGameObject* pGameObject)
+{
+}
+
+void CField_Item::RenderFont()
+{
+	__super::Late_Update(0.15f);
 }
 
 CField_Item* CField_Item::Create(LPDIRECT3DDEVICE9 pGraphic_Device, LEVEL eLevel)
