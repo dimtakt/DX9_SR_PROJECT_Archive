2ef80c61a1654fce54ebf57b39397ed48c418c28
XDXDX-99 <dmsql002925@gmail.com>
2025-06-15 11:07:23 +0900

    MERGE

--- Framework/Client/Private/Erma.cpp
+++ Framework/Client/Private/Erma.cpp
@@ -8,6 +8,7 @@
 #include "Interaction_Normal.h"
 #include "Room_Manager.h"
 #include "GameInstance.h"
+#include "Client_Struct.h"
 
 CErma::CErma(LPDIRECT3DDEVICE9 pGraphic_Device)
 	: CMonster{ pGraphic_Device }
